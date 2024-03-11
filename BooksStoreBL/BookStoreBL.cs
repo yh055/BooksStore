07761e79fb2ca1c24f6febae471e39942afecc2a
Yael Hadad <yael0556734@gmail.com>
2024-03-12 01:12:25 +0200

    Add project files.

---
+++ BooksStoreBL/BookStoreBL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksStoreDAL;
+using Entities;
+
+
+namespace BooksStoreBL
+{
+    public class BookStoreBL
+    {
+        List<BookDetails> ListOfBooks;
+        FileConnection fileConnection;
+        public BookStoreBL()
+        {
+            fileConnection = new FileConnection();
+            ListOfBooks = fileConnection.ReadAllBooks();
+        }
+
+        public IEnumerable<BookDetails> GetAllBooks()
+        {
+            return ListOfBooks;
+        }
+        public IEnumerable<BookDetails> GetAllBooksPriceMore30()
+        {
+            return ListOfBooks.Where(x => x.Price > 30); 
+        }
+        public IEnumerable<BookDetails> GetAllBooksSort()
+        {
+            return ListOfBooks.OrderBy(x => x.Id);
+        }
+        public IEnumerable<BookDetails> GetAllBooksPriceOfComiks(/*List<BookDetails> l*/)
+        {
+
+            return /*(IEnumerable<BookDetails>)*/ListOfBooks.Where(c => c.IsComics == true).Select(p => p.Price);
+        }
+   
+        public IEnumerable<BookDetails> GetAllBooksNameOf9()
+        {
+
+            return ListOfBooks./*Select(p=>p.Name).*/Where(c => c.MaxAge >= 9&& c.MinAge <= 9);
+        }
+     
+    }
+}
---
+++ Entities/BookDetails.cs
@@ -0,0 +1,25 @@
+﻿namespace Entities
+{
+    public class BookDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int NumberOfPages { get; set; }
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+        public bool IsComics { get; set; }
+
+        public BookDetails(int id, string name, int price, int numberOfPages, 
+            int minAge, int maxAge, bool isComics)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+            this.NumberOfPages = numberOfPages;
+            this.MinAge = minAge;
+            this.MaxAge = maxAge;
+            this.IsComics = isComics;
+        }
+    }
+}
